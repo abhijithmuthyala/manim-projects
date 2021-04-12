11da3ef064000326cebe2dbe89f0f2b63dac11fb
Abhijith Muthyala <73361366+abhi3019@users.noreply.github.com>
2021-04-12 13:54:47 +0530

    Add perp line length indicators and their updaters

--- mini_projects/viviani_theorem.py
+++ mini_projects/viviani_theorem.py
@@ -25,3 +25,22 @@ def right_angle_updater(right_angle, perp_line):
     anchors = right_angle.get_anchors()
     right_angle.shift(perp_line.points[-1] - (anchors[-1] + (anchors[0] - anchors[1])))
     return right_angle
+
+
+def get_length_indicators(perp_lines, width, dl_corner, corner_radius=0.05, **kwargs):
+    rects = VGroup()
+    for line in perp_lines:
+        rect = RoundedRectangle(
+            corner_radius, width=width, height=line.get_length(), color=line.get_color()
+        )
+        rects += rect
+    rects.arrange(DOWN, 0).next_to(dl_corner, buff=0, aligned_edge=DOWN)
+    rects.set_style(**kwargs)
+    rects.dl_corner = dl_corner
+    return rects
+
+
+def length_indicators_updater(rects, perp_lines):
+    for rect, line in zip(rects, perp_lines):
+        rect.stretch_to_fit_height(line.get_length())
+    return rects.arrange(DOWN, 0).next_to(rects.dl_corner, buff=0, aligned_edge=DOWN)
