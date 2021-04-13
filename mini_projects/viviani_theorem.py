ebc4b3fa6c6dc07a41f55a0b3426e6f09591ca6a
Abhijith Muthyala <73361366+abhi3019@users.noreply.github.com>
2021-04-13 16:23:12 +0530

    Essence done, proof left

--- mini_projects/viviani_theorem.py
+++ mini_projects/viviani_theorem.py
@@ -46,6 +46,12 @@ def length_indicators_updater(rects, perp_lines):
     return rects.arrange(DOWN, 0).next_to(rects.dl_corner, buff=0, aligned_edge=DOWN)
 
 
+def get_triangle_to_edge(edge_index, eq_triangle, point, **kwargs):
+    vertices = eq_triangle.get_vertices()
+    vertices = np.append(vertices, [vertices[0]], 0)
+    return Polygon(point, *vertices[edge_index : edge_index + 2], **kwargs)
+
+
 config.background_color = "#111111"
 
 
@@ -60,7 +66,7 @@ class VivianiTheorem(Scene):
                 "interior point to the sides, is equal to its altitude.",
                 line_spacing=0.8,
                 font=fonts[0],
-            ).set_opacity(0.2)
+            ).set_opacity(0.35)
 
         self.wait(0.25)
         self.play(FadeInFromLarge(title, 1.15, run_time=4, lag_ratio=0.1))
@@ -68,7 +74,7 @@ class VivianiTheorem(Scene):
         self.play(title.animate.scale(0.5).to_corner(UL, buff=0.25))
         self.wait(0.25)
 
-        theorem.set_width(config.frame_width - 1)
+        theorem.set(width=config.frame_width - 1)
         theorem.next_to(title, DOWN, buff=0.5, aligned_edge=LEFT)
         triangle = EquilateralTriangle(4.5, color=GOLD_E, fill_opacity=0.1)
         triangle.next_to(theorem, DOWN, buff=0.5)
@@ -83,16 +89,17 @@ class VivianiTheorem(Scene):
         self.wait(0.5)
 
         dot = Dot(point=triangle.circumcenter)
-        blob = Blob(triangle.inradius - 0.1, max_scale=1, n_samples=15)
-        blob.shift(triangle.circumcenter)
         perp_line_colors = [RED_C, PINK, BLUE_E]  # ccw starting from right edge
         perp_lines = []
+        right_angles = []
         for i, color in zip([2, 0, 1], perp_line_colors):
             line = triangle.get_perpendicular_line_to_edge(
                 i, dot.get_center(), color=color
             )
             line.z_index = -1
-            perp_lines += line
+            right_angle = get_right_angle_to_edge(line, 0.125).match_style(line)
+            perp_lines.append(line)
+            right_angles.append(right_angle)
 
         self.play(
             FadeInFromLarge(dot, 3),
@@ -104,6 +111,7 @@ class VivianiTheorem(Scene):
             run_time=3,
         )
         self.play(*map(Create, perp_lines), run_time=0.75)
+        self.play(*map(Create, right_angles), run_time=0.75)
 
         length_indicators = get_length_indicators(
             perp_lines,
@@ -117,16 +125,59 @@ class VivianiTheorem(Scene):
         self.play(Restore(length_indicators, run_time=2))
         self.wait(0.5)
 
+        altitude_ref_lines = []
+        for i, corner in zip([0, 1], [UR, DR]):
+            start = triangle.get_vertices()[i]
+            end = length_indicators.get_corner(corner)
+            altitude_ref_lines.append(DashedLine(start, end))
 
         self.play(
-            dot.animate.move_to(blob.points[0]), 
-            *map(lambda l:UpdateFromFunc(l, lambda l: perp_line_updater(l, triangle, dot)), perp_lines)
+            *map(Create, altitude_ref_lines),
+            theorem[1][13:].animate(lag_ratio=1).set_opacity(1),
+            run_time=3
         )
-        self.play(
-            theorem[1][13:].animate(lag_ratio=1, run_time=2).set_opacity(1),
-            AnimationGroup(
-                MoveAlongPath(dot, blob, rate_func=double_smooth), 
-                *map(lambda l:UpdateFromFunc(l, lambda l: perp_line_updater(l, triangle, dot)), perp_lines), 
-                run_time=10
-        ))
         self.wait()
+
+        updater_anims = [
+            *map(
+                lambda l: UpdateFromFunc(
+                    l, lambda l: perp_line_updater(l, triangle, dot)
+                ),
+                perp_lines,
+            ),
+            UpdateFromFunc(
+                length_indicators, lambda li: length_indicators_updater(li, perp_lines)
+            ),
+            UpdateFromFunc(
+                right_angles[0],
+                lambda r: right_angle_updater(right_angles[0], perp_lines[0]),
+            ),
+            UpdateFromFunc(
+                right_angles[1],
+                lambda r: right_angle_updater(right_angles[1], perp_lines[1]),
+            ),
+            UpdateFromFunc(
+                right_angles[2],
+                lambda r: right_angle_updater(right_angles[2], perp_lines[2]),
+            ),
+        ]
+
+        for _ in range(2):
+            blob = Blob(triangle.inradius - 0.125, max_scale=0.95, n_samples=15)
+            blob.shift(triangle.circumcenter)
+            self.play(
+                dot.animate.move_to(blob.points[0]),
+                *updater_anims,
+                rate_func=linear,
+                run_time=2
+            )
+            self.play(
+                MoveAlongPath(dot, blob), *updater_anims, rate_func=linear, run_time=8
+            )
+
+        self.play(
+            dot.animate.move_to(triangle.get_center()),
+            *updater_anims,
+            rate_func=linear,
+            run_time=2
+        )
